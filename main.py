6bcd7bdfd7b0dcf3d87d2b613a05f8e4d8a24d8d
h3nnn4n <uber.renan@gmail.com>
2021-12-21 19:54:51 -0300

    Parse request body

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ logger.setLevel(logging.INFO)
 
 
 def handler(event, context):
-    logger.debug("call")
-    response = {"statusCode": 200, "body": "hi"}
+    logger.debug(f"{event=}")
+    raw_request_body = event.get("body")
+    logger.debuginfo(f"{raw_request_body=}")
+    request_body = json.loads(raw_request_body)
+
+    body = {"event": request_body}
+    response = {"statusCode": 200, "body": json.dumps(body)}
     return response
